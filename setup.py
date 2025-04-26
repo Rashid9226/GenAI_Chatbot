ff71ccf6a6e7b9fb677bff986f65cb5d60a6d894
Muhammed Rashid <muhammedrashid7274@gmail.com>
2025-04-26 18:04:00 +0530

    setup file uploaded

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI',
+    version='0.0',
+    author='rash d data',
+    author_email="raashidmuhammed01@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
+
