2da8f6fe892fd1fff4ed9dafcdfeb77e5fa05463
Muhammed Rashid <103042046+Rashid9226@users.noreply.github.com>
2025-04-26 18:52:39 +0530

    Update port code

--- app.py
+++ app.py
@@ -185,6 +185,13 @@ async def health():
     return {"status": "ok"}
 
 
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
 
 #-----------------------------------#
 
